d79ec9753acc70d5b3d62303f5e8327464be8d50
Manuel <manuel.salvi@ko2.it>
2022-06-19 14:30:06 +0200

    lambda

--- section.js
+++ section.js
@@ -139,10 +139,3 @@ function get_glitched_tip(e, i) {
     }
     return glitched_hint_html_fn(e, offset_x, offset_y);
 }
-
-document.querySelector("#terminal_input").addEventListener('keypress', function (e) {
-    console.log(e);
-    if(e.code == 13){
-        play();
-    }
-});
