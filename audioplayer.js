61d61218e88ee790007efdcd90ae8909454b6ee0
Manuel <manuel.salvi@ko2.it>
2022-06-27 01:01:00 +0200

    audioloop preload test

--- audioplayer.js
+++ audioplayer.js
@@ -6,14 +6,17 @@ class FadeInLoop {
   
     constructor(ctx, url) {
       this.ctx = ctx;
-      this.audioBuffer = fetch(url)
-        .then(response => response.arrayBuffer())
-        .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer));
-  
+      this.audioBuffer = get_audio_buffer(url);
       this.gainNode = ctx.createGain();
       this.gainNode.connect(ctx.destination);
     }
-  
+    
+    async get_audio_buffer(url) {
+        return await fetch(url)
+            .then(response => response.arrayBuffer())
+            .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer));
+    }
+
     async start() {
       this.isPlaying = true;
       const source = ctx.createBufferSource();
@@ -25,7 +28,7 @@ class FadeInLoop {
       })
   
       source.connect(this.gainNode);
-      source.buffer = await this.audioBuffer;
+      source.buffer = this.audioBuffer;
       const now = this.ctx.currentTime;
       this.gainNode.gain.setValueAtTime(Number.EPSILON, now);
       this.gainNode.gain.exponentialRampToValueAtTime(1, now + 0.055);
@@ -39,10 +42,6 @@ class FadeInLoop {
   }
   
   function switch_audio(exit_loop, entry_loop) {
-    exit_loop.stop().then(() => entry_loop.start());
+    if(exit_loop.isPlaying && !entry_loop.isPlaying)
+        exit_loop.stop().then(() => entry_loop.start());
   }
-
-  const ctx = new AudioContext({ latencyHint: 'interactive' })
-  const first_loop = new FadeInLoop(ctx, 'https://audio.jukehost.co.uk/jdWaU4FggWzEnDzqrXbwVt5p1ZByzlqq')  
-  const second_loop = new FadeInLoop(ctx, 'https://audio.jukehost.co.uk/jdWaU4FggWzEnDzqrXbwVt5p1ZByzlqq')  
-  const third_loop = new FadeInLoop(ctx, 'https://audio.jukehost.co.uk/jdWaU4FggWzEnDzqrXbwVt5p1ZByzlqq')  
